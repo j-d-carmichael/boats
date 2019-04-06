cb718185fa921a28b71eaade4d60adbb6fb1ad0d
John carmichael <johncarmichael1984@gmail.com>
2019-04-07 08:42:30 +1000

    options update

--- index.js
+++ index.js
@@ -5,18 +5,27 @@ const pkg = require('./package.json')
 program
   .version(pkg.version, '-v, --version')
   .option('--init', 'Inject a skeleton yml structure to the current directory named /src/...')
-  .option('-i, --input [path]', 'The relative path to the input file')
-  .option('-c, --clean_leaf', 'This will strip all trailing "," from all values')
-  .option('-d, --destination_name [name]', 'Base name of the file eg "weather_api". The version number from the swagger file will be appended automatically unless instructed otherwise')
-  .option('-D, --destination [path]', 'Path to the target eg "./build". If no destination directory is passed the output will be outputted in the terminal')
-  .option('-e, --extension [ext]', 'The output extension, defaults to the output format if not provided')
-  .option('-o, --output_format [format]', 'The output format yaml, yml or json. If not provided it will assume the format of the input file')
-  .option('-n, --indentation [indent]', 'The numeric indentation, defaults to 4 if nothing passed')
-  .option('-m, --make_unique_operation_ids', '// WARNING: modifies your files, check with git: Changes the value of all operationId to the camelCase pathname of the file minus the dir path then continues to the usual operation of bundling.')
-  .option('-M, --make_unique_operation_ids_only', 'Same as -m but will only inject the uniqueOperationIds into the yaml file and then stop')
-  .option('-s, --strip_value [strip]', 'Related to m & M, the value removed from the file path for the uniqueIds, defaults to src/paths/')
-  .option('-V, --validate_off', 'Do not validate the swagger output')
-  .option('-x, --exclude_version', 'Exclude the swagger version from the generated output file')
+
+  .option('-i, --input [path]', 'The relative path to the main input file eg "./src/index.yml"')
+
+  .option('-o, --output [path]', 'Path to the target eg "./build/weather_api". The version of the api will be appended automatically based on the version from within the OA index if present. If option not passed the output will be in the terminal.')
+
+  .option('-x, --exclude_version', 'Exclude the OA version from the generated output file.')
+
+  .option('-f, --output_format [format]', 'The output format yaml, yml or json. If not provided it will assume the format of the input file')
+
+  .option('-e, --extension [ext]', 'The output extension, defaults to the output format if not provided. This is typically used to for the "yml" or "yaml" ext.')
+
+  .option('-I, --indentation [indent]', 'The numeric indentation, defaults to 2 if option passed')
+
+  .option('-u, --unique_operation_ids', '// WARNING: modifies your files, check with git: Changes the value of all operationId to the camelCase pathname of the file minus the dir path then continues to the usual operation of bundling.')
+
+  .option('-U, --unique_operation_ids_only', 'Same as -m but will only inject the uniqueOperationIds into the yaml file and then stop')
+
+  .option('-s, --strip_value [strip]', 'Related to u & U, the value removed from the file path for the uniqueIds, defaults to src/paths/')
+
+  .option('-v, --validate [state]', 'Validate OA 2/3 state "on" or "off". Defaults to "on"')
+
   .parse(process.argv)
 
 // Initialise a code base and exit
