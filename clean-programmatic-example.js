ce8ed8a77f5223966e7bff6eea3e178febce5157
John carmichael <johncarmichael1984@gmail.com>
2019-04-10 13:03:27 +1000

    additional notes

--- clean-programmatic-example.js
+++ clean-programmatic-example.js
@@ -4,7 +4,9 @@ const bundler = new Bundler({
   input: './srcOA3/index.yml',
   customVars: {
     host: 'http://www.somehost.com'
-  }
+  },
+  // You can add any of the full text commands here as key value pairs for example:
+  validate: 'off'
 })
 
 // Writes the output to file
