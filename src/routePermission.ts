ccdb2b32a26aa920624da74c48f508cfd5744856
Patrick McGowan <patrick.mcgowan@acrontum.de>
2020-07-13 19:14:27 +0200

    lint and typings

--- src/routePermission.ts
+++ src/routePermission.ts
@@ -1,10 +1,12 @@
-import path from 'path'
-import ucFirst from '@/ucFirst'
-import UniqueOperationIds from '@/UniqueOperationIds'
+import path from 'path';
+import ucFirst from '@/ucFirst';
+import UniqueOperationIds from '@/UniqueOperationIds';
+import { JSON, BoatsRC } from '@/interfaces/generic';
 
-export default (boatsrc: any, filePath: string, stripValue: string, tail: string) => {
-  const permissionConfig = boatsrc && boatsrc.permissionConfig && boatsrc.permissionConfig.routePrefix
-  const prefixConfig = Object.assign(
+export default (boatsrc: BoatsRC, filePath: string, stripValue: string, tail: string): string => {
+  const permissionConfig =
+    boatsrc && boatsrc.permissionConfig && boatsrc.permissionConfig.routePrefix;
+  const prefixConfig: JSON = Object.assign(
     {
       get: 'read',
       post: 'create',
@@ -13,10 +15,10 @@ export default (boatsrc: any, filePath: string, stripValue: string, tail: string
       delete: 'delete',
     },
     permissionConfig
-  )
-  const opId = UniqueOperationIds.getUniqueOperationIdFromPath(filePath, stripValue, tail)
-  const method = path.basename(filePath).replace(/\..*/, '').toLowerCase()
-  const prefix = prefixConfig[method] || method
+  );
+  const opId = UniqueOperationIds.getUniqueOperationIdFromPath(filePath, stripValue, tail);
+  const method = path.basename(filePath).replace(/\..*/, '').toLowerCase();
+  const prefix = prefixConfig[method] || method;
 
-  return `${prefix}${ucFirst(opId)}`
-}
+  return `${prefix}${ucFirst(opId)}`;
+};
