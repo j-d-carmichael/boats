48c336336043cb2526f090fa85067f908522c210
Patrick McGowan <patrick.mcgowan@acrontum.de>
2020-10-06 17:45:32 +0200

    fmt

--- src/nunjucksHelpers/autoTag.ts
+++ src/nunjucksHelpers/autoTag.ts
@@ -2,14 +2,12 @@ import ucFirst from '@/ucFirst';
 
 export default function (): string {
   const base = this.env.globals.currentFilePointer.replace(this.env.globals.uniqueOpIdStripValue, '').split('/');
-  let tag
+  let tag;
   switch (base.length) {
     case 1:
       tag = base[0].split('.').shift();
     default:
       tag = ucFirst(base[0]);
   }
-  return tag
-    .replace('{', '')
-    .replace('}', '')
+  return tag.replace('{', '').replace('}', '');
 }
