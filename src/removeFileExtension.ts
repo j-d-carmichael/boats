a2c1617c505eebb4b9bcb5c434d41ed2d0d7e7e4
Kévin DUBUC <kevindubuc62@gmail.com>
2024-05-22 12:07:50 +0200

    Ignore yaml extension

--- src/removeFileExtension.ts
+++ src/removeFileExtension.ts
@@ -3,5 +3,6 @@ import stripFromEndOfString from '@/stripFromEndOfString';
 export default (filePath: string): string => {
   filePath = stripFromEndOfString(filePath, '.njk');
   filePath = stripFromEndOfString(filePath, '.yml');
+  filePath = stripFromEndOfString(filePath, '.yaml');
   return filePath;
 };
