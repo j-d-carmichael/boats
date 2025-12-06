ed32e2b7f51d7fd2936300eb9d543d8d6a826d88
John Carmichael <johncarmichael1984@gmail.com>
2025-12-06 16:40:43 +0000

    chore: deprecated code form v4 removed from .boatsrc options around the permissions helper

--- src/__tests__/GetCheckCorrectBoatsRc.spec.ts
+++ src/__tests__/GetCheckCorrectBoatsRc.spec.ts
@@ -16,26 +16,3 @@ it('should warn about the invalid string style types', async () => {
   });
   expect(global.console.warn).toHaveBeenCalled();
 });
-
-it('should warn and write old config naming to new', async () => {
-  // @ts-ignore
-  global.console = {
-    warn: jest.fn(),
-  };
-  const newRc = CheckBoatsRcAndCorrect.parse({
-    nunjucksOptions: {
-      tags: {},
-    },
-    permissionConfig: {
-      usePackageJsonNameAsPrefix: true,
-      routePrefix: {},
-    },
-    fancyPluralization: true
-  });
-  expect(global.console.warn).toHaveBeenCalled();
-  expect(newRc.permissionConfig.usePackageJsonNameAsPrefix).toBe(undefined);
-  expect(newRc.permissionConfig.globalPrefix).toBe(true);
-  expect(newRc.permissionConfig.routePrefix).toBe(undefined);
-  expect(newRc.permissionConfig.methodAlias).toEqual({});
-  expect(newRc.fancyPluralization).toEqual(true);
-});
